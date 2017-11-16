62dcaeab546024608da83d3da6736d1540e9fa6b
Caroline Heymes <caroline.heymes@gmail.com>
2017-11-16 09:24:39 +0000

    creation des routes et mise en place de la background imageavec gradient linéaire

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :flynn]
 
   def home
+  end
+  def flynn
   end
 end
